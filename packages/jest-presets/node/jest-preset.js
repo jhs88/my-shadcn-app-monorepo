81b39c38ca8cc75305072c7f18e7e2cf70803b55
Joseph Scherreik <joe.scherreik@gmail.com>
2025-03-28 14:26:35 -0400

    tests now working

---
+++ packages/jest-presets/node/jest-preset.js
@@ -0,0 +1,15 @@
+export const config = {
+  roots: ["<rootDir>"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/test/__fixtures__",
+    "<rootDir>/node_modules",
+    "<rootDir>/dist",
+  ],
+  preset: "ts-jest",
+};
+
+export default config;
