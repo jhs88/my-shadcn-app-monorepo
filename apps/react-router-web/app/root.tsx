20425a6aff3fc42ce4f91b3437bfab1e6d9186f3
Joseph Scherreik <joe.scherreik@gmail.com>
2025-04-18 16:28:22 -0400

    fixed FOUT

--- apps/react-router-web/app/root.tsx
+++ apps/react-router-web/app/root.tsx
@@ -22,7 +22,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=auto",
   },
 ];
 
@@ -35,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="font-sans antialiased ">
         <Providers>{children}</Providers>
         <ScrollRestoration />
         <Scripts />
