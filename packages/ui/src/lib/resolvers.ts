520051240960db6f607859f3638a89742d110446
Joseph Scherreik <joe.scherreik@gmail.com>
2025-06-18 13:02:52 -0400

    remove resolvers

--- packages/ui/src/lib/resolvers.ts
+++
@@ -1,5 +0,0 @@
-// Export a global instance resolver for @hookform/resolvers
-// If issues persist, consider installing @hookform/resolvers directly in your app
-export * from "@hookform/resolvers/zod";
-
-export type * from "@hookform/resolvers/zod";
