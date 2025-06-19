d383ec254dfbbf17a6ba1d0d718625fa397f305f
Joseph Scherreik <joe.scherreik@gmail.com>
2025-06-19 15:36:08 -0400

    current user image

---
+++ apps/web/components/current-user-avatar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useCurrentUserImage } from "@/hooks/use-current-user-image";
+import { useCurrentUserName } from "@/hooks/use-current-user-name";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@repo/ui/components/avatar";
+
+export const CurrentUserAvatar = () => {
+  const profileImage = useCurrentUserImage();
+  const name = useCurrentUserName();
+  const initials = name
+    ?.split(" ")
+    ?.map((word) => word[0])
+    ?.join("")
+    ?.toUpperCase();
+
+  return (
+    <Avatar>
+      {profileImage && <AvatarImage src={profileImage} alt={initials} />}
+      <AvatarFallback>{initials}</AvatarFallback>
+    </Avatar>
+  );
+};
