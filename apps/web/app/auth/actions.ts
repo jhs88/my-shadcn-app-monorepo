6e6b0ec6169c76f65653ab55f05829ba76257e49
Joseph Scherreik <joe.scherreik@gmail.com>
2025-08-07 21:49:01 -0400

    even faster login?

--- apps/web/app/auth/actions.ts
+++ apps/web/app/auth/actions.ts
@@ -17,19 +17,6 @@ export async function signup(formData: FormData) {
   redirect("/protected");
 }
 
-export async function login(formData: FormData) {
-  const supabase = await createClient();
-
-  const { error } = await supabase.auth.signInWithPassword({
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  });
-  if (error) redirect("/auth/error");
-
-  revalidatePath("/", "layout");
-  redirect("/protected");
-}
-
 export async function oauthLogin(origin: string) {
   const supabase = await createClient();
 
@@ -40,10 +27,4 @@ export async function oauthLogin(origin: string) {
 
   if (error) redirect("/auth/error");
   if (data.url) redirect(data.url);
-}
-
-export async function logout() {
-  const supabase = await createClient();
-  await supabase.auth.signOut();
-  redirect("/auth/login");
 }
