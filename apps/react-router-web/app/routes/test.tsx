7a88b2977cd40d9f5ed770620b4f518225e444d4
Joseph Scherreik <joe.scherreik@gmail.com>
2026-02-16 21:05:36 -0500

    add image support

--- apps/react-router-web/app/routes/test.tsx
+++ apps/react-router-web/app/routes/test.tsx
@@ -1,3 +1,8 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@repo/ui/components/avatar";
 import { Button } from "@repo/ui/components/button";
 import {
   Dialog,
@@ -6,33 +11,133 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@repo/ui/components/dialog";
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemHeader,
+  ItemMedia,
+  ItemTitle,
+} from "@repo/ui/components/item";
+import { ScrollArea } from "@repo/ui/components/scroll-area";
+import { redirect, type LoaderFunctionArgs } from "react-router";
 import { toast } from "sonner";
-import { CurrentUserAvatar } from "~/components/current-user-avatar";
 import Navbar from "~/components/navbar";
+import { createClient } from "~/lib/supabase/server";
+import type { Route } from "./+types/test";
 
-export default function Index() {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { supabase } = createClient(request);
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) return redirect("/login");
+
+  const email: string | undefined = data?.user?.email;
+  const name: string = data?.user?.user_metadata.full_name ?? email;
+  const emailVerified: string = data?.user?.user_metadata.email_verified;
+  const profileImage: string | undefined = data?.user?.user_metadata.avatar_url;
+
+  const { data: profiles } = await supabase
+    .from("profiles")
+    .select()
+    .filter("id", "eq", data?.user?.id);
+
+  const profile = profiles ? profiles[0] : undefined;
+  const initials = name
+    ?.split(" ")
+    ?.map((word) => word[0])
+    ?.join("")
+    ?.toUpperCase();
+
+  return { email, emailVerified, name, initials, profile, profileImage };
+};
+
+export default function Test({ loaderData }: Route.ComponentProps) {
+  const { email, emailVerified, name, initials, profile, profileImage } =
+    loaderData;
+
   return (
     <>
       <Navbar />
       <main className="container mx-auto min-h-screen w-full overflow-hidden">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="hover:cursor-pointer"
-            >
-              <CurrentUserAvatar />
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Profile</DialogTitle>
-            </DialogHeader>
-            My Profile Info
-          </DialogContent>
-        </Dialog>
-        <Button onClick={() => toast("My first toast")}>Give me a toast</Button>
+        <div className="flex w-full gap-6">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                size="icon-sm"
+                variant="ghost"
+                className="hover:cursor-pointer"
+              >
+                <Avatar>
+                  <AvatarImage
+                    className="object-cover"
+                    src={profile?.avatar_url ?? profileImage}
+                    alt={initials ?? "?"}
+                  />
+                  <AvatarFallback>{initials ?? "?"}</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="min-h-60 w-full">
+              <DialogHeader>
+                <DialogTitle>My Profile Info</DialogTitle>
+              </DialogHeader>
+              <ScrollArea className="max-h-140 w-full">
+                <ItemGroup className="gap-2">
+                  <Item variant="outline">
+                    <ItemHeader>
+                      <ItemTitle>Email</ItemTitle>
+                      <ItemDescription>
+                        <small className="float-right text-sm font-medium leading-none">
+                          {email ?? "No User Logged In"}
+                        </small>
+                      </ItemDescription>
+                    </ItemHeader>
+                  </Item>
+                  <Item variant="outline">
+                    <ItemHeader>
+                      <ItemTitle>Is Verified</ItemTitle>
+                      <ItemDescription>
+                        <small className="float-right text-sm font-medium leading-none">
+                          {emailVerified ? "yes" : "no"}
+                        </small>
+                      </ItemDescription>
+                    </ItemHeader>
+                  </Item>
+                  <Item variant="outline">
+                    <ItemHeader>
+                      <ItemTitle>Username</ItemTitle>
+                      <ItemDescription>
+                        <small className="float-right text-sm font-medium leading-none">
+                          {profile?.username ?? "No Username Found"}
+                        </small>
+                      </ItemDescription>
+                    </ItemHeader>
+                  </Item>
+                  <Item variant="outline">
+                    <ItemContent>
+                      <ItemTitle>Avatar</ItemTitle>
+                      <ItemDescription></ItemDescription>
+                    </ItemContent>
+                    <ItemHeader>
+                      <ItemMedia variant="image" className="h-fit w-full">
+                        <img
+                          className="object-cover"
+                          src={profile?.avatar_url ?? profileImage}
+                          alt={initials ?? "?"}
+                        />
+                      </ItemMedia>
+                    </ItemHeader>
+                  </Item>
+                </ItemGroup>
+              </ScrollArea>
+            </DialogContent>
+          </Dialog>
+          <Button onClick={() => toast("My first toast")}>
+            Give me a toast
+          </Button>
+        </div>
       </main>
     </>
   );
--- apps/web/app/test/page.tsx
+++ apps/web/app/test/page.tsx
@@ -1,6 +1,4 @@
 import PageContainer from "@/components/layout/page-container";
-import { LoginForm } from "@/components/login-form";
-import { LogoutButton } from "@/components/logout-button";
 import { createClient } from "@/lib/supabase/server";
 import {
   Avatar,
@@ -8,25 +6,36 @@ import {
   AvatarImage,
 } from "@repo/ui/components/avatar";
 import { Button } from "@repo/ui/components/button";
-import { Card, CardContent } from "@repo/ui/components/card";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@repo/ui/components/dialog";
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemHeader,
+  ItemMedia,
+  ItemTitle,
+} from "@repo/ui/components/item";
+import { ScrollArea } from "@repo/ui/components/scroll-area";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.getUser();
-  // if (error || !data?.user) redirect("/auth/login");
+  if (error || !data?.user) redirect("/auth/login");
 
   const email: string | undefined = data?.user?.email;
   const name: string = data?.user?.user_metadata.full_name ?? email;
-  const profileImage: string = data?.user?.user_metadata.avatar_url;
+  const emailVerified: string = data?.user?.user_metadata.email_verified;
+  const profileImage: string | undefined =
+    data?.user?.user_metadata.avatar_url ?? "";
 
   const { data: profiles } = await supabase
     .from("profiles")
@@ -46,7 +55,10 @@ export default async function Page() {
         <DialogTrigger asChild>
           <Button variant="ghost" size="icon" className="hover:cursor-pointer">
             <Avatar>
-              <AvatarImage src={profileImage} alt={initials ?? "?"} />
+              <AvatarImage
+                src={profile?.avatar_url ?? profileImage}
+                alt={initials ?? "?"}
+              />
               <AvatarFallback>{initials ?? "?"}</AvatarFallback>
             </Avatar>
           </Button>
@@ -55,29 +67,55 @@ export default async function Page() {
           <DialogHeader>
             <DialogTitle>My Profile Info</DialogTitle>
           </DialogHeader>
-          <Card>
-            <CardContent>
-              <DialogDescription className="text-lg font-semibold">
-                Email:
-                <small className="float-right text-sm font-medium leading-none">
-                  {email ?? "No User Logged In"}
-                </small>
-              </DialogDescription>
-              <DialogDescription className="text-lg font-semibold">
-                Is Verified:
-                <small className="float-right text-sm font-medium leading-none">
-                  {data?.user?.user_metadata.email_verified ? "yes" : "no"}
-                </small>
-              </DialogDescription>
-              <DialogDescription className="text-lg font-semibold">
-                Username:
-                <small className="float-right text-sm font-medium leading-none">
-                  {profile?.username ?? "No Username Found"}
-                </small>
-              </DialogDescription>
-            </CardContent>
-          </Card>
-          {error || !data?.user ? <LoginForm /> : <LogoutButton />}
+          <ScrollArea className="max-h-140 w-full">
+            <ItemGroup className="gap-2">
+              <Item variant="outline">
+                <ItemHeader>
+                  <ItemTitle>Email</ItemTitle>
+                  <ItemDescription>
+                    <small className="float-right text-sm font-medium leading-none">
+                      {email ?? "No User Logged In"}
+                    </small>
+                  </ItemDescription>
+                </ItemHeader>
+              </Item>
+              <Item variant="outline">
+                <ItemHeader>
+                  <ItemTitle>Is Verified</ItemTitle>
+                  <ItemDescription>
+                    <small className="float-right text-sm font-medium leading-none">
+                      {emailVerified ? "yes" : "no"}
+                    </small>
+                  </ItemDescription>
+                </ItemHeader>
+              </Item>
+              <Item variant="outline">
+                <ItemHeader>
+                  <ItemTitle>Username</ItemTitle>
+                  <ItemDescription>
+                    <small className="float-right text-sm font-medium leading-none">
+                      {profile?.username ?? "No Username Found"}
+                    </small>
+                  </ItemDescription>
+                </ItemHeader>
+              </Item>
+              <Item variant="outline">
+                <ItemContent>
+                  <ItemTitle>Avatar</ItemTitle>
+                  <ItemDescription></ItemDescription>
+                </ItemContent>
+                <ItemHeader>
+                  <ItemMedia variant="image" className="h-fit w-full">
+                    <img
+                      className="object-cover"
+                      src={profile?.avatar_url ?? profileImage}
+                      alt={initials ?? "?"}
+                    />
+                  </ItemMedia>
+                </ItemHeader>
+              </Item>
+            </ItemGroup>
+          </ScrollArea>
         </DialogContent>
       </Dialog>
     </PageContainer>
